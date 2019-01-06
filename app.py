a6065387074c27b0ea217aa7a0b5198fadd91427
MrCharlss <camachotcarlos@gmail.com>
2019-01-06 17:20:58 -0600

    Request inicial funcionando

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    cats = requests.get("https://api.chucknorris.io/jokes/categories")
+    jCats = cats.json()
+    for cat in jCats:
+        print(cat)
+
+    return jCats[2]
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
